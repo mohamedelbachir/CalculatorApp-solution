ac6859b2a6955576525b3b5420559be277e05edd
mohamedelbachir <mohamedelbachirboubanganadakou@gmail.com>
2023-05-11 09:13:28 +0200

    some bug fix and style

--- src/Components/Screen.component.jsx
+++ src/Components/Screen.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../styles/screen.style.scss'
-export default function Screen({value}) {
+export default function Screen({expression,value}) {
     return (
-        <div className="screen">
+        <div className="screen" data-action={expression}>
             <input type="text" name="result" id="result" disabled={true} value={value}/>
         </div>
     )
